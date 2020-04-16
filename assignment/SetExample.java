f0206e8d0d3e05435cac378de70718d4bf078711
bhanu345-cmd <bhanuprathusha@gmail.com>
2020-04-16 15:19:10 +0530

    commited by bhanu

---
+++ assignment/SetExample.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class SetExample {
+
+	public static void main(String[] args) {
+
+
+		 LinkedHashSet<Integer> list =  new LinkedHashSet<Integer>();
+		 list.add(20);
+		 list.add(50);
+		 list.add(22);
+		 list.add(50);
+		 System.out.println(list);
+		 
+		
+
+	}
+
+}
+//Collection , ArrayList ,  Sorting, Handson
